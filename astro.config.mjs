a2855afb36e95f7252f843b424243a714a25620e
aximoliya <1509643183@qq.com>
2026-05-29 09:25:09 +0800

    feat: 优化 SEO + Sitemap + PWA 支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   site: 'https://aximoliya.github.io/DaoYou',
   base: '/DaoYou',
+  output: 'static',
   build: {
     assets: '_assets'
   },
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+      lastmod: new Date(),
+    })
+  ],
   vite: {
     build: {
       cssMinify: true,
